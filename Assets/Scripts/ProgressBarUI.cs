5000de4c6286fee1b0a1d8c256ac26e4690e0b3c
Jose Simoes <jose.simooes@gmail.com>
2023-04-04 13:22:44 +0900

    feat: add stove counter with cooking

--- Assets/Scripts/ProgressBarUI.cs
+++ Assets/Scripts/ProgressBarUI.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ProgressBarUI : MonoBehaviour {
-    [SerializeField] private CuttingCounter cuttingCounter;
+    [SerializeField] private GameObject hasProgressGameObject;
+    private IHasProgress hasProgress;
     [SerializeField] private Image barImage;
 
     private void Start() {
-        cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+        hasProgress = hasProgressGameObject.GetComponent<IHasProgress>();
+        if (hasProgress == null) {
+            Debug.LogError("GameObject " + hasProgressGameObject + " does not have a component that implements IHasProgress!");
+        }
+        hasProgress.OnProgressChanged += CuttingCounter_OnProgressChanged;
         barImage.fillAmount = 0f;
         Hide();
     }
-    private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e) {
+    private void CuttingCounter_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e) {
         barImage.fillAmount = e.progressNormalized;
 
         if (e.progressNormalized == 0f || e.progressNormalized == 1f) {
